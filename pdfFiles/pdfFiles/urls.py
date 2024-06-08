e4f3abc6ab71c44314ddb568b8be4e66891dca06
Bonheur <bonheurmafoundou@gmail.com>
2024-06-08 13:23:14 +0100

    set home page

--- pdfFiles/pdfFiles/urls.py
+++ pdfFiles/pdfFiles/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include, re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions, routers
+from django.conf.urls.static import static
+from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -24,6 +26,5 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
 ]
