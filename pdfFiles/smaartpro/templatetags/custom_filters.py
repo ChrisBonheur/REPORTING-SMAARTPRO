ae5ba844deed4b11702e60ae8e6528df0d92deef
ChrisBonheur <bonheurmafoundou@gmail.com>
2024-09-08 10:15:26 +0100

    remain card

--- pdfFiles/smaartpro/templatetags/custom_filters.py
+++ pdfFiles/smaartpro/templatetags/custom_filters.py
@@ -6,10 +6,10 @@ register = template.Library()
 def get_item(dictionary, key):
     return dictionary.get(key)
 
-"""
+
 @register.filter
 def chunk_list(value, chunk_size):
+    """Divise une liste en sous-listes de taille chunk_size."""
     if not isinstance(value, list):
         return value
     return [value[i:i + chunk_size] for i in range(0, len(value), chunk_size)]
-"""
