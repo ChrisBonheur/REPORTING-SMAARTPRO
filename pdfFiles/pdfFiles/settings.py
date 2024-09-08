494079f8d4d679c9dab2970847ef5edf9cbca243
ChrisBonheur <bonheurmafoundou@gmail.com>
2024-09-08 01:05:10 +0100

    set

--- pdfFiles/pdfFiles/settings.py
+++ pdfFiles/pdfFiles/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'corsheaders',
     "rest_framework",
     'drf_yasg',
-    'smaartpro'
+    'smaartpro',
 ]
 
 MIDDLEWARE = [
@@ -134,7 +134,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOW_HEADERS = ['*']
+CSRF_TRUSTED_ORIGINS = ['*']
 
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
